9a4b9e1abe172b1292b420df4a4644bd2a796d1c
Ilyas Ronef <ilyas@DivanDesign.ru>
2022-08-02 14:32:54 +0300

    * Minor changes.

--- ddSendRedirect_plugin.php
+++ ddSendRedirect_plugin.php
@@ -1,14 +1,13 @@
 //<?php
 /** 
- * ddRedirectMoved plugin
+ * ddSendRedirect
  * @version 1.0 (2012-02-16)
  * 
- * Переадресовывает несуществующие страницы на необходимые адреса (url или id документа).
+ * @desc Переадресовывает несуществующие страницы на необходимые адреса (url или id документа).
  * 
  * @events OnPageNotFound
  * 
- * @copyright 2012, DivanDesign
- * http://www.DivanDesign.ru
+ * @copyright 2012 DD Group {@link https://DivanDesign.biz }
  */
 
 $rules = array(
@@ -19,15 +18,26 @@ $e = &$modx->Event;
 
 if ($e->name == 'OnPageNotFound'){
 	$oldUrl = $_SERVER['REQUEST_URI'];
-
+	
 	//Если для текущего url есть правило  
 	if (isset($rules[$oldUrl])){
-		//Если редиректить надо на id, сформируем url
+		//Если редиректить надо на ID, сформируем url
 		if (is_numeric($rules[$oldUrl])){
-			$rules[$oldUrl] = $modx->makeUrl($rules[$oldUrl], '', '', 'full');
+			$rules[$oldUrl] = $modx->makeUrl(
+				$rules[$oldUrl],
+				'',
+				'',
+				'full'
+			);
 		}
 		
-		$this->sendRedirect($rules[$oldUrl], 0, 'REDIRECT_HEADER', 'HTTP/1.1 301 Moved Permanently');
+		$this->sendRedirect(
+			$rules[$oldUrl],
+			0,
+			'REDIRECT_HEADER',
+			'HTTP/1.1 301 Moved Permanently'
+		);
+		
 		exit;
 	}
 }
